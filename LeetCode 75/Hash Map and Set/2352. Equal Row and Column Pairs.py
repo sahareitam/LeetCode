987a2974efcde95fb92447de6eede7093eff9a47
Sahar Eitam <sahar283@gmail.com>
2025-01-08 18:17:37 +0200

    add 2352. Equal Row and Column Pairs question

--- LeetCode 75/Hash Map and Set/2352. Equal Row and Column Pairs.py
+++ LeetCode 75/Hash Map and Set/2352. Equal Row and Column Pairs.py
@@ -1,22 +1,12 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        counter_col = []
-        counter_row = []
-        for _ in range(n):
-            counter_col.append([])
 
-        for r in range(n):
-            counter_row.append(str(grid[r]))
-            for c in range(n):
-                counter_col[c].append(grid[r][c])
-        for c in range(n):
-            counter_col[c] = str(counter_col[c])
+        for i in range(n):
+            grid[i] = tuple(grid[i])
 
-        counter_row = Counter(counter_row)
-        counter_col = Counter(counter_col)
-        counter = 0
-        for col in counter_col:
-            if col in counter_row:
-                counter += counter_col[col]*counter_row[col]
-        return counter
+        counter = Counter(grid)
+        count = 0
+        for col in zip(*grid):
+            count += counter[col]
+        return count
