632f58d38d8afdc65aabb04edc67820c715da863
Sahar Eitam <sahar283@gmail.com>
2025-02-18 12:36:15 +0200

    add question

--- NeetCode/496. Next Greater Element I.py
+++ NeetCode/496. Next Greater Element I.py
@@ -5,16 +5,6 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        # n = len(nums2)
-        # for i,num in enumerate(nums1):
-        #     checker =nums2.index(num)
-        #     while checker < n-1 and num >= nums2[checker]:
-        #         checker += 1
-        #     if nums2[checker] > num:
-        #         nums1[i] = nums2[checker]
-        #     else:
-        #         nums1[i] = -1
-        # return nums1
         stack = []
         hashmap = {}
         res = []
