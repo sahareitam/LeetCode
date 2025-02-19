ef58401fff492a69a4560ca7b4a9abfaf3160319
Sahar Eitam <sahar283@gmail.com>
2025-02-19 19:58:27 +0200

    add question

---
+++ NeetCode/75. Sort Colors.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = len(nums)-1
+        i=0
+
+        while i<=r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l+=1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                i-=1
+                r-=1
+            i+=1
+        return nums
