86527065b1d9a185ca6de5497e47b15965266f62
Sahar Eitam <sahar283@gmail.com>
2025-03-20 15:59:44 +0200

    add question

---
+++ Grind 75/56. Merge Intervals.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res = []
+        intervals.sort(key=lambda x: x[0])
+        res.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if res[-1][1] >= intervals[i][0]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            else:
+                res.append(intervals[i])
+
+        return res
+
